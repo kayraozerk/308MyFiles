3aa35984205c7c12cc6d46318531cea9319e6489
kayra ozerk <kayra.ozerk@sabanciuniv.edu>
2024-03-09 13:45:30 +0300

    first files created

---
+++ plane.js
@@ -0,0 +1,43 @@
+class Plane {
+  constructor(
+    planeID,
+    planeType,
+    crewLimit,
+    passengerLimit,
+    seatCapacity,
+    seniorityRestriction
+  ) {
+    this.planeID = planeID;
+    this.planeType = planeType;
+    this.crewLimit = crewLimit;
+    this.passengerLimit = passengerLimit;
+    this.seatCapacity = seatCapacity;
+    this.seniorityRestriction = seniorityRestriction;
+  }
+
+  displayInfo() {
+    console.log(`Plane ID: ${this.planeID}`);
+    console.log(`Plane Type: ${this.planeType}`);
+    console.log(`Passenger Limit: ${this.passengerLimit}`);
+    console.log(`Crew Limit: ${this.crewLimit}`);
+    console.log(`seatCapacity: ${this.seatCapacity}`);
+    console.log(`Plane Seniority Level: ${this.seniorityRestriction}`);
+  }
+}
+
+module.exports = { Plane, displayInfo };
+
+/*
+CREATE TABLE Plane{
+  PlaneID CHAR(6) NOT NULL,
+  planeType CHAR(20) NOT NULL,
+  CrewLimit INTEGER NOT NULL,
+  PassengerLimit INTEGER NOT NULL,
+  SeatCapacity INTEGER NOT NULL,
+  seniorityRestiriction INTEGER NOT NULL, // 1-2-3-4 gibi tek haneli bir sayı olacak
+
+  PRIMARY KEY(PlaneID)
+}
+
+
+*/
